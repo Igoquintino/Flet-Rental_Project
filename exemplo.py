7f3debbea74a85f313e9c5bc87ba9f31c32e3249
Igo Quintino <igocastro.15@gmail.com>
2024-12-18 16:12:45 -0300

    algumas adições, conteine page e modificações, ainda em teste

--- exemplo.py
+++ exemplo.py
@@ -1,7 +1,14 @@
 import flet as ft
 
 def main(page: ft.Page):
-    #adiciona controlles na pagina
-    pass
+    # adiciona controlles na pagina
+    page.padding = 0
+    page.window_width = 400
+    page.window_height = 600  
+    
+    page.controls.append(ft.Text("Hello World"))
+    page.update()
+    # se quiser, pode usar sem ta colocando .control.apend assim: page.add(ft.Text("insira o texto aqui"))
+    
 
-ft.app(target=main)
+ft.app(port= 8550, target=main)
